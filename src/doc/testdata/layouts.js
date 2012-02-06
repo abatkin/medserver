60b2a51ff319cf12e6c0e52e1efeb111aaadaa6e
Adam Batkin <adam@batkin.net>
2012-02-06 11:47:10 -0500

    slightly better sample data

--- src/doc/testdata/layouts.js
+++ src/doc/testdata/layouts.js
@@ -1,46 +1,56 @@
 {
-        "formName" : "testForm",
-        "schemaName" : "testSchema",
-        "description" : "This is a test form",
-        "title" : "Test Form",
-        "links" : [
-                {
-                	name: "about",
-                	displayText: "About",
-                	linkType: "DataLink",
-                	displayData: "This is a big form for you to fill out. We like forms. Please fill out the whole thing."
-                },
-                {
-                	name: "github",
-                	displayText: "GitHub",
-                	linkType: "ExternalLink",
-                	destinationUrl: "https://github.com/abatkin/medserver"
-                }
-        ],
-        "sections" : [
-                {
-                        "title" : "Section One",
-                        "instructions" : "This is section one. Please fill out all of the information",
-                        "fields" : [
-                                {
-                                		"widgetType" : "string",
-                                        "name" : "firstName",
-                                        "title" : "First Name",
-                                        "helpText" : "Please enter your first name"
-                                },
-                                {
-                                		"widgetType" : "string",
-                                        "name" : "lastName",
-                                        "title" : "Last Name",
-                                        "helpText" : "Please enter your last name"
-                                },
-                                {
-                                		"widgetType" : "boolean",
-                                		"name" : "isAwesome",
-                                		"title" : "Are you awesome",
-                                		"helpText" : "Check here if you are awesome"
-                                }
-                        ]
-                }
-        ]
+	"formName" : "testForm",
+	"schemaName" : "testSchema",
+	"description" : "This is a test form",
+	"title" : "Test Form",
+	"links" : [
+		{
+			"name" : "about",
+			"displayText" : "About",
+			"linkType" : "DataLink",
+			"displayData" : "This is a big form for you to fill out. We like forms. Please fill out the whole thing."
+		},
+		{
+			"name" : "github",
+			"displayText" : "GitHub",
+			"linkType" : "ExternalLink",
+			"destinationUrl" : "https://github.com/abatkin/medserver"
+		}
+	],
+	"sections" : [
+		{
+			"title" : "Section One",
+			"instructions" : "This is section one. Please fill out all of the information",
+			"fields" : [
+				{
+					"widgetType" : "string",
+					"name" : "firstName",
+					"title" : "First Name",
+					"helpText" : "Please enter your first name"
+				},
+				{
+					"widgetType" : "string",
+					"name" : "lastName",
+					"title" : "Last Name",
+					"helpText" : "Please enter your last name"
+				},
+				{
+					"widgetType" : "integer",
+					"name" : "age",
+					"title" : "Age"
+				},
+				{
+					"widgetType" : "boolean",
+					"name" : "isAwesome",
+					"title" : "Are you awesome",
+					"helpText" : "Check here if you are awesome"
+				},
+				{
+					"widgetType" : "dropdown",
+					"name" : "coverage",
+					"title" : "Select Something"
+				}
+			]
+		}
+	]
 }
--- src/doc/testdata/schemas.js
+++ src/doc/testdata/schemas.js
@@ -1,18 +1,36 @@
 {
-        "schemaName" : "testSchema",
-        "fields" : [
-                {
-                        "dataType" : "string",
-                        "name" : "firstName",
-                },
-                {
-                        "dataType" : "string",
-                        "name" : "lastName",
-                },
-                {
-                		"dataType" : "boolean",
-                		"name" : "isAwesome"
-                }
-        ],
-        "active" : true
+	"schemaName" : "testSchema",
+	"fields" : [
+		{
+			"dataType" : "string",
+			"name" : "firstName"
+		},
+		{
+			"dataType" : "string",
+			"name" : "lastName"
+		},
+		{
+			"dataType" : "integer",
+			"name" : "age"
+		},
+		{
+			"dataType" : "boolean",
+			"name" : "isAwesome"
+		},
+		{
+			"dataType" : "selectFromOptions",
+			"name" : "coverage",
+			"optionValues" : [
+				{
+					"value" : NumberInt(0),
+					"displayText" : "First Option"
+				},
+				{
+					"value" : NumberInt(1),
+					"displayText" : "Second Option"
+				}
+			]
+		}
+	],
+	"active" : true
 }
